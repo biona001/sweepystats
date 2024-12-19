a214594967cb6844183311dd396dff31afe90fe6
Benjamin Chu <benchu99@hotmail.com>
2024-12-19 15:55:07 -0800

    sigma2, coef_std, cov

--- sweepy/linreg.py
+++ sweepy/linreg.py
@@ -27,20 +27,43 @@ class LinearRegression:
         self.fitted = False
 
     def fit(self, verbose=True):
+        """Perform least squares fitting by sweep operation"""
         if not self.fitted:
             p = self.A.shape[0] - 1
             for k in tqdm(range(p), disable = not verbose):
                 self.A.sweep_k(k)
             self.fitted = True
         return None
-    
+
     def coef(self, verbose=True):
+        """Fitted coefficient values (beta hat)"""
         if not self.fitted:
             self.fit(verbose=verbose)
-        return self.A.A[0:self.p, -1].copy()
+        return self.A[0:self.p, -1].copy()
+    
+    def coef_std(self, verbose=True):
+        """Standard deviation of the fitted coefficient values"""
+        if not self.fitted:
+            self.fit(verbose=verbose)
+        sigma2 = self.sigma2()
+        beta_var = np.diagonal(self.A.A).copy()[0:-1]
+        return np.sqrt(-sigma2 * beta_var)
 
     def resid(self, verbose=True):
+        """Estimate of residuals = ||y - yhat||^2"""
         if not self.fitted:
             self.fit(verbose=verbose)
-        return self.A.A[-1, -1]
-    
+        return self.A[-1, -1]
+
+    def sigma2(self, verbose=True):
+        """Estimate of sigma square."""
+        if not self.fitted:
+            self.fit(verbose=verbose)
+        n, p = self.n, self.p
+        return self.resid() / (n - p)
+
+    def cov(self, verbose=True):
+        """Estimated variance-covariance of beta hat, i.e. Var(b) = sigma2 * inv(X'X)"""
+        if not self.fitted:
+            self.fit(verbose=verbose)
+        return -self.sigma2() * self.A[0:-1, 0:-1].copy()
--- tests/test_linreg.py
+++ tests/test_linreg.py
@@ -2,14 +2,19 @@ import numpy as np
 import sweepy as sw
 
 def test_linreg():
-    X = np.random.rand(5, 3)
-    y = np.random.rand(5)
+    n, p = 5, 3
+    X = np.random.rand(n, p)
+    y = np.random.rand(n)
     ols = sw.LinearRegression(X, y)
     ols.fit()
-    beta, resid, _, _ = np.linalg.lstsq(X, y) # least squares solution by QR
 
-    # beta hat
-    np.allclose(ols.coef(), beta)
+    # least squares solution by QR
+    beta, resid, _, _ = np.linalg.lstsq(X, y)
+    sigma2 = resid[0] / (n - p)
+    beta_cov = sigma2 * np.linalg.inv(X.T @ X)
+    beta_std = np.sqrt(np.diag(beta_cov))
 
-    # residual
-    np.allclose(ols.resid(), resid)
+    assert np.allclose(ols.coef(), beta)         # beta hat
+    assert np.allclose(ols.resid(), resid)       # residual
+    assert np.allclose(ols.cov(), beta_cov)      # Var(beta hat)
+    assert np.allclose(ols.coef_std(), beta_std) # std of beta hat
