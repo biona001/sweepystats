b50bee258f7b0770b9f47e9d64c6bf7eb2ca54cd
Benjamin Chu <benchu99@hotmail.com>
2024-12-21 15:04:58 -0800

    correct package build

--- sweepystats/__init__.py
+++ sweepystats/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 
-from .sweepy import SweepMatrix
+from .sweep_matrix import SweepMatrix
 from .linreg import LinearRegression
 
 __all__ = ["SweepMatrix", "LinearRegression"]
