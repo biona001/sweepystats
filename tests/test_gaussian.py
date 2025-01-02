cd187f36e1020de70b64c290c90640fb31b6161b
Benjamin Chu <benchu99@hotmail.com>
2025-01-02 15:36:32 -0800

    add cond var tests

--- tests/test_gaussian.py
+++ tests/test_gaussian.py
@@ -62,3 +62,24 @@ def test_cond_expectation():
     sigma_ZY = sigma[np.ix_(zidx, yidx)]
     ans_true = mu_Z + sigma_ZY @ np.linalg.inv(sigma_Y) @ (y - mu_Y)
     assert np.allclose(ans, ans_true)
+
+def test_cond_var():
+    # instantiate a Normal
+    p = 6
+    mu = np.zeros(p)
+    sigma = toeplitz(np.array([0.5**i for i in range(p)]))
+    d = sw.Normal(mu, sigma)
+
+    # sweepystats cond var
+    y = np.random.normal(2, size=(2,))
+    yidx = [0, 2]
+    ans = d.cond_var(y, yidx)
+
+    # check answers with brute-force implementation
+    zidx = [1, 3, 4, 5]
+    sigma_Y = sigma[np.ix_(yidx, yidx)]
+    sigma_Z = sigma[np.ix_(zidx, zidx)]
+    sigma_ZY = sigma[np.ix_(zidx, yidx)]
+    sigma_YZ = sigma[np.ix_(yidx, zidx)]
+    ans_true = sigma_Z - sigma_ZY @ np.linalg.inv(sigma_Y) @ sigma_YZ
+    np.allclose(ans, ans_true)
